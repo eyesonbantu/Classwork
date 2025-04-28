192ad2d5e0584f69f5e54dd80393451fb8c596a5
eyesonbantu <mwendwar363@gmail.com>
2025-04-28 12:08:39 +0300

    Second Commit

---
+++ OOP.py
@@ -0,0 +1,17 @@
+class ShoppingCart:
+
+ def add_item(self,age,count):
+     self.count = count
+     self.age = age
+     self.bool1 = "True"
+     bool1 = True
+
+     print(f"{age}, {count},{bool1}")
+
+obj1: ShoppingCart = ShoppingCart()
+obj2: ShoppingCart = ShoppingCart()
+obj2.bool1 = False
+
+
+obj1.add_item(20,1)
+obj2.add_item(20,1)
---
+++ OOP1.py
@@ -0,0 +1,48 @@
+from tabnanny import check
+
+class ShoppingCart:
+
+    def __init__(self):
+     self.items = []
+
+    def add_items(self,item_name:str,qty:int,unit_price: float):
+     #"methods"
+     self.items.append((item_name,qty,unit_price))
+
+    def remove_item(self,item_name:str):
+
+      for item in self.items:
+          if item[0] == item_name:
+              self.items.remove(item)
+
+    def calculate_total(self) -> float:
+      total = 0.0
+
+      for name,qty,price in self.items:
+          total += qty * price
+      return total
+
+    def summary (self):
+
+      print("Cart Contents")
+      for name, qty , price in self.items:
+          print(f"{name}: ,{qty} @ Kshs{price:.3f}each")
+      print(f"Total:Ksh{self.calculate_total():.2f}\n")
+
+
+def checkout(cart: ShoppingCart):
+ cart.summary()
+ print(f"Final amount: Ksh {cart.calculate_total():.2f}\n")
+
+#usage
+
+if __name__ == "__main__":
+  cart: ShoppingCart = ShoppingCart()
+  cart.add_items("Kiwi",50,79.2)
+  cart.add_items("Papaya",30,50.1)
+  cart.add_items("Guava",10,20.1)
+  cart.add_items("Mango",50,7.6)
+
+  print(">>> Regular Cart<<<")
+
+  checkout(cart)
